3e41d20bf571f18664f6d8125c356647594678a7
SantosN21 <santosjnovoa21@gmail.com>
2023-04-05 22:13:09 -0700

    Stylings and html

---
+++ script.js
@@ -0,0 +1,8 @@
+const calcInput = document.querySelector('.result');
+const allButtons = document.querySelectorAll('.button');
+
+allButtons.forEach(button => button {
+    button.addEventListener('click', (e) => {
+        
+    }
+})

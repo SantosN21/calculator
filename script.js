80eff825a2f051a5622371421008d9accf1c7109
SantosN21 <santosjnovoa21@gmail.com>
2023-04-06 22:06:18 -0700

    All buttons working normally except for the +/-

--- script.js
+++ script.js
@@ -1,8 +1,58 @@
-const calcInput = document.querySelector('.result');
+const result = document.querySelector('.result');
 const allButtons = document.querySelectorAll('.button');
 
-allButtons.forEach(button => button {
+
+allButtons.forEach(button => {
     button.addEventListener('click', (e) => {
-        
-    }
+        const value = e.target.dataset.value;
+        result.innerText += value;
+        if (value == '=') {
+            if (result.innerText.includes('/')) {
+                const splitStuff = result.innerText.split('/');
+                operate('/', splitStuff[0], splitStuff[1])
+            }
+            if (result.innerText.includes('-')) {
+                const splitStuff = result.innerText.split('-');
+                operate('-', splitStuff[0], splitStuff[1])
+            }
+            if (result.innerText.includes('+')) {
+                const splitStuff = result.innerText.split('+');
+                operate('+', splitStuff[0], splitStuff[1])
+            }
+            if (result.innerText.includes('*')) {
+                const splitStuff = result.innerText.split('*');
+                operate('*', splitStuff[0], splitStuff[1])
+            }
+            if (result.innerText.includes('%')) {
+                const splitStuff = result.innerText.split('%');
+                operate('%', splitStuff[0], splitStuff[1])
+            }
+        }
+        if (value === 'C') {
+            result.innerText = "";
+        }
+    })
 })
+
+const operate = (operator, valueOne, valueTwo) => {
+        numOne = parseInt(valueOne);
+        numTwo = parseInt(valueTwo);
+    if (operator === '/') {
+        result.innerText = numOne / numTwo;
+    }
+    if (operator === '-') {
+        result.innerText = numOne - numTwo;
+    }
+    if (operator === '+') {
+        console.log(parseInt(valueOne + valueTwo))
+        result.innerText = numOne + numTwo;
+    }
+    if (operator === '*') {
+        result.innerText = numOne * numTwo;
+    }
+    if (operator === '%') {
+        result.innerText = numOne % numTwo;
+    }
+}
+
+
